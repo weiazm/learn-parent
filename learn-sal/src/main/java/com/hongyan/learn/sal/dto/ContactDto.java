8a3367cac382477ca2750ce57d0c8abac9b7bc91
魏鸿焱 <weihongyan@baijiahulian.com>
2016-08-27 10:13:38 +0800

    x

---
+++ learn-sal/src/main/java/com/hongyan/learn/sal/dto/ContactDto.java
@@ -0,0 +1,21 @@
+/**
+ * Baijiahulian.com Inc.
+ * Copyright (c) 2014-2016 All Rights Reserved.
+ */
+package com.hongyan.learn.sal.dto;
+
+import lombok.Data;
+
+/**
+ * @title ContactDto
+ * @desc 1+1=2
+ * @author hongyan
+ * @date Aug 27, 2016
+ * @version 1.0
+ */
+@Data
+public class ContactDto {
+    private Integer id;
+    private String name;
+    private String mobile;
+}
