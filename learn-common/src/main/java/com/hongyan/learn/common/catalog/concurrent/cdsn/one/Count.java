b3a9a338bf6f85aa4ed8bb16b6fc0ff563759e39
鸿焱 <weihongyan@baijiahulian.com>
2016-08-28 18:44:44 +0800

    x

---
+++ learn-common/src/main/java/com/hongyan/learn/common/catalog/concurrent/cdsn/one/Count.java
@@ -0,0 +1,21 @@
+/**
+ * Baijiahulian.com Inc.
+ * Copyright (c) 2014-2016 All Rights Reserved.
+ */
+package com.hongyan.learn.common.catalog.concurrent.cdsn.one;
+
+/**
+ * @title Count
+ * @desc description
+ * @author weihongyan
+ * @date Aug 28, 2016
+ * @version version
+ */
+public class Count {
+    public Count(int a, int b){
+        
+    }
+    void count(){
+        
+    }
+}
---
+++ learn-common/src/main/java/com/hongyan/learn/common/catalog/proxy/homework/ClassProxyFactoryImpl.java
@@ -0,0 +1,11 @@
+package com.hongyan.learn.common.catalog.proxy.homework;
+
+public class ClassProxyFactoryImpl implements ClassProxyFactory {
+
+    @Override
+    public Object createProxy(Object originalInstance, ClassProxy proxy) throws Throwable {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
