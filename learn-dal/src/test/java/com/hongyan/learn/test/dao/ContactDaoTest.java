558c2c89ecd672b139619c8f8c85c4cab04ab8d3
鸿焱 <weihongyan@baijiahulian.com>
2016-08-29 11:31:27 +0800

    dance

--- learn-dal/src/test/java/com/hongyan/learn/test/dao/ContactDaoTest.java
+++ learn-dal/src/test/java/com/hongyan/learn/test/dao/ContactDaoTest.java
@@ -4,6 +4,16 @@
  */
 package com.hongyan.learn.test.dao;
 
+import com.hongyan.learn.dal.dao.ContactDao;
+import com.hongyan.learn.dal.po.Contact;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @title ContactDaoTest
  * @desc description
@@ -11,6 +21,18 @@ package com.hongyan.learn.test.dao;
  * @date Aug 29, 2016
  * @version version
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:application-config.xml")
 public class ContactDaoTest {
-
+    @Autowired
+    private ContactDao contactDao;
+    
+    @Test
+    public void testGetContactByName(){
+        String name = "张三";
+        Contact contact = contactDao.getByName(name);
+        Assert.assertNotNull(contact);
+        Assert.assertEquals(contact.getName(), name);
+        System.out.println(contact);
+    }
 }
