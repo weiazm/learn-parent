023f6995f159d9788966f9cec989f47fc321e338
鸿焱 <weihongyan@baijiahulian.com>
2016-08-29 11:04:58 +0800

    x

---
+++ learn-dal/src/test/java/com/hongyan/learn/test/dao/ContactDaoTest.java
@@ -0,0 +1,16 @@
+/**
+ * Baijiahulian.com Inc.
+ * Copyright (c) 2014-2016 All Rights Reserved.
+ */
+package com.hongyan.learn.test.dao;
+
+/**
+ * @title ContactDaoTest
+ * @desc description
+ * @author weihongyan
+ * @date Aug 29, 2016
+ * @version version
+ */
+public class ContactDaoTest {
+
+}
