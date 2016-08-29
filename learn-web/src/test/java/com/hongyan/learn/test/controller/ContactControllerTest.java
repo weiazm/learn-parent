5d3a77d7ff98fedfaf251ce4fa38337778e6318c
鸿焱 <weihongyan@baijiahulian.com>
2016-08-29 13:09:28 +0800

    done

---
+++ learn-web/src/test/java/com/hongyan/learn/test/controller/ContactControllerTest.java
@@ -0,0 +1,45 @@
+/**
+ * Baijiahulian.com Inc.
+ * Copyright (c) 2014-2016 All Rights Reserved.
+ */
+package com.hongyan.learn.test.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * @title ContactControllerTest
+ * @desc description
+ * @author weihongyan
+ * @date Aug 29, 2016
+ * @version version
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:application-config.xml","classpath:mvc-config.xml"})
+public class ContactControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+    
+    @Before
+    public void setup(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    
+    @Test
+    public void contactControllerTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/contact/getByName.json?contactName=张三"))
+            .andDo(MockMvcResultHandlers.print());
+    }
+}
