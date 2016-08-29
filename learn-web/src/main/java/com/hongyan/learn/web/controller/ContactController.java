b66bf38b65bbf592a816ba5df9dfca2cacb94686
鸿焱 <weihongyan@baijiahulian.com>
2016-08-29 13:36:59 +0800

    x

--- learn-web/src/main/java/com/hongyan/learn/web/controller/ContactController.java
+++ learn-web/src/main/java/com/hongyan/learn/web/controller/ContactController.java
@@ -11,8 +11,6 @@ import com.hongyan.learn.web.dto.ErrorDetail.ErrorCode;
 import com.hongyan.learn.web.dto.ResponseStatus;
 import com.hongyan.learn.web.dto.WebResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
