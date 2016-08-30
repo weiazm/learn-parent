26161112b65978b112a42deb9383e58b6478b1af
鸿焱 <weihongyan@baijiahulian.com>
2016-08-30 19:53:09 +0800

    x

---
+++ learn-common/src/test/java/com/hongyan/learn/test/util/RedisTemplateTest.java
@@ -0,0 +1,24 @@
+/**
+ * Baijiahulian.com Inc.
+ * Copyright (c) 2014-2016 All Rights Reserved.
+ */
+package com.hongyan.learn.test.util;
+
+/**
+ * @title RedisTemplateTest
+ * @desc description
+ * @author weihongyan
+ * @date Aug 30, 2016
+ * @version version
+ */
+public class RedisTemplateTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
