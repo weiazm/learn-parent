622f579dae4fcdc12bb6a13e31e5356780169e88
鸿焱 <weihongyan@baijiahulian.com>
2016-08-30 13:33:35 +0800

    done

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-tomcatWebappsPath="/Users/weihongyan/Documents/workspace/apache-tomcat-9.0.0.M9/webapps"
-targetWarPath="/Users/weihongyan/Documents/workspace/learn-parent/learn-web/target"
-warName="learn"
+# tomcatWebappsPath="/Users/weihongyan/Documents/workspace/apache-tomcat-8.5.4/webapps"
+# targetWarPath="/Users/weihongyan/Documents/workspace/learn-parent/learn-web/target"
+# warName="learn"
 
-cd "${targetWarPath}/../../"
-mvn clean package -DskipTests=true
-rm -rf "${tomcatWebappsPath}/${warName}"
-rm -rf "${tomcatWebappsPath}/${warName}.war"
-mv "${targetWarPath}/${warName}.war" "${tomcatWebappsPath}/${warName}.war"
+# cd "${targetWarPath}/../../"
+# mvn clean package -DskipTests=true
+# rm -rf "${tomcatWebappsPath}/${warName}"
+# rm -rf "${tomcatWebappsPath}/${warName}.war"
+# mv "${targetWarPath}/${warName}.war" "${tomcatWebappsPath}/${warName}.war"
+
+mvn clean install
+cd learn-web
+mvn cargo:run
+cd ..
