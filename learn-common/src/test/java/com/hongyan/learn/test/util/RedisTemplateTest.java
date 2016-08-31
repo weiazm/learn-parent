ba509223516957050cdd5493e30f57a9589604fd
鸿焱 <weihongyan@baijiahulian.com>
2016-08-31 13:52:29 +0800

    x

--- learn-common/src/test/java/com/hongyan/learn/test/util/RedisTemplateTest.java
+++ learn-common/src/test/java/com/hongyan/learn/test/util/RedisTemplateTest.java
@@ -7,7 +7,11 @@ package com.hongyan.learn.test.util;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -24,7 +28,10 @@ public class RedisTemplateTest {
 
 
     @Autowired
-    RedisTemplate<String, String> jedisTemplate;
+    private RedisTemplate<String, String> jedisTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     public void putAndGet() {
@@ -32,6 +39,18 @@ public class RedisTemplateTest {
         Object name = jedisTemplate.opsForHash().get("user", "name");
         System.out.println(name);
         jedisTemplate.opsForHash().delete("user", "name");
+    }
+
+    @Test
+    public void redisCallBackTest() {
+        String res = stringRedisTemplate.execute(new RedisCallback<String>() {
+            @Override
+            public String doInRedis(RedisConnection connection) throws DataAccessException {
+                connection.setEx("张三".getBytes(), 2L, "经纪人".getBytes());
+                return new String(connection.get("张三".getBytes()));
+            }
+        });
+        System.out.println(res);
     }
 
 }
