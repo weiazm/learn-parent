fb6e6862bfcc678399b99c5531ac8a429658445a
鸿焱 <weihongyan@baijiahulian.com>
2016-08-31 15:21:35 +0800

    x

--- learn-web/src/test/java/com/hongyan/learn/test/controller/ContactControllerTest.java
+++ learn-web/src/test/java/com/hongyan/learn/test/controller/ContactControllerTest.java
@@ -4,6 +4,12 @@
  */
 package com.hongyan.learn.test.controller;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
 /**
  * @title ContactControllerTest
  * @desc description
@@ -12,8 +18,17 @@ package com.hongyan.learn.test.controller;
  * @version version
  */
 public class ContactControllerTest extends SuperControllerTest {
+    private static ApplicationContext context;
+
     @Override
     public void test() {
-            this.pathPrintTest("/contact/getByName.json?contactName=张三");
+        this.pathPrintTest("/contact/getByName.json?contactName=张三");
+    }
+
+    @Test
+    public void test2() {
+        context = new ClassPathXmlApplicationContext("application-config.xml");
+        StringRedisTemplate redisTemplate = (StringRedisTemplate) context.getBean("redisTemplate");
+        System.out.println(((JedisConnectionFactory) (redisTemplate.getConnectionFactory())).getPassword());
     }
 }
