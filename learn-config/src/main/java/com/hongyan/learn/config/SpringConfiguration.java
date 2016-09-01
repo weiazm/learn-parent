26a09a5a0bb90d9b14bedd861051375dabbb8bdf
鸿焱 <weihongyan@baijiahulian.com>
2016-09-01 10:26:34 +0800

    x

---
+++ learn-config/src/main/java/com/hongyan/learn/config/SpringConfiguration.java
@@ -0,0 +1,16 @@
+/**
+ * Baijiahulian.com Inc.
+ * Copyright (c) 2014-2016 All Rights Reserved.
+ */
+package com.hongyan.learn.config;
+
+/**
+ * @title SpringConfiguration
+ * @desc thebestisyettocome
+ * @author weihongyan
+ * @date Sep 1, 2016
+ * @version null
+ */
+public class SpringConfiguration {
+
+}
