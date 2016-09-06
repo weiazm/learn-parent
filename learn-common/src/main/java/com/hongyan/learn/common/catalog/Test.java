7469303b6bc45eeaf566aa61420ce5589848d46d
鸿焱 <weihongyan@baijiahulian.com>
2016-09-06 17:25:54 +0800

    fuck 啊

---
+++ learn-common/src/main/java/com/hongyan/learn/common/catalog/Test.java
@@ -0,0 +1,21 @@
+/**
+ * Baijiahulian.com Inc.
+ * Copyright (c) 2014-2016 All Rights Reserved.
+ */
+/**
+ * @title package-info
+ * @desc 1+1=2
+ * @author hongyan
+ * @date Aug 27, 2016
+ * @version 1.0
+ */
+package com.hongyan.learn.common.catalog;
+
+public class Test {
+    public static void main(String[] args) {
+        Integer a = new Integer(1);
+        Integer b = new Integer(1);
+        System.out.println(a.equals(b));
+        System.out.println(a == b);
+    }
+}
