af7319051487c65c601e9d0ce8f24923986c2099
鸿焱 <weihongyan@baijiahulian.com>
2016-09-07 09:28:06 +0800

    x

--- learn-common/src/main/java/com/hongyan/learn/common/catalog/Test.java
+++ learn-common/src/main/java/com/hongyan/learn/common/catalog/Test.java
@@ -17,5 +17,12 @@ public class Test {
         Integer b = new Integer(1);
         System.out.println(a.equals(b));
         System.out.println(a == b);
+
+        String s1 = "Programming";
+        String s2 = new String("Programming");
+        String s3 = "Program" + "ming";
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s1.intern());
     }
 }
