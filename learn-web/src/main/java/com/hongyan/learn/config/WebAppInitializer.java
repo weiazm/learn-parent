fa1738e13d04912da3ee14efbbe031fd635e6c00
鸿焱 <weihongyan@baijiahulian.com>
2016-09-09 20:23:41 +0800

    添加编码filter

--- learn-web/src/main/java/com/hongyan/learn/config/WebAppInitializer.java
+++ learn-web/src/main/java/com/hongyan/learn/config/WebAppInitializer.java
@@ -4,7 +4,10 @@
  */
 package com.hongyan.learn.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 /**
  * Created by weihongyan on 9/9/16.
@@ -12,16 +15,24 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{RootConfig.class};
+        return new Class<?>[]{ApplicationConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[]{MvcConfig.class, ApplicationConfig.class};
+        return new Class<?>[]{MvcConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return new Filter[]{filter};
     }
 }
